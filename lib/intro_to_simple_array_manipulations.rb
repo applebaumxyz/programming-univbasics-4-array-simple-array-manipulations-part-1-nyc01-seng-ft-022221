45876ff75f4c987e5c6a4116b844023d52983b64
Sean Brown <applebaumxyz@gmail.com>
2021-02-17 00:55:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,13 @@
+=begin
+artists = ["Hozier", "Ariana Grande", "Usher"]
+next_artist = "Beyonce"
+artists.push(next_artist)
+# => ["Hozier", "Ariana Grande", "Usher", "Beyonce"]
+=end
+
+def using_push(array, string)
+ array = ["i", "think", "this", "is"]
+ string = ["easy"]
+ array.push("easy")
+end
+
